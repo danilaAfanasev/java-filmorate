6ed7aecbebdac31185714327bf3055cc75442db3
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-09-26 15:46:39 +0300

    Создан класс Film

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,33 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
 
-/**
- * Film.
- */
-@Getter
-@Setter
-public class Film {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+@Data
+@Builder(toBuilder = true)
+@AllArgsConstructor
+public class Film extends Model {
+
+    private int id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Size(max = 200)
+    private String description;
+
+    @NotNull
+    private LocalDate releaseDate;
+
+    @PositiveOrZero
+    private int duration;
 }
