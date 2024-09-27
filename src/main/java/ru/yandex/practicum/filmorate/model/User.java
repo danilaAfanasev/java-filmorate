b893f49d920ff3e4aaf5e07f5ba9b7bfca39d09c
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-09-27 16:34:06 +0300

    Добавлен конструктор с полем id из класса предка

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
@@ -15,9 +16,8 @@ import java.time.LocalDate;
 @Data
 @Builder(toBuilder = true)
 @AllArgsConstructor
+@NoArgsConstructor
 public class User extends Model {
-
-    private int id;
 
     @NotBlank
     @Email
@@ -30,4 +30,12 @@ public class User extends Model {
     @PastOrPresent
     @NotNull
     private LocalDate birthday;
+
+    public User(int id, String email, String login, String name, LocalDate birthday) {
+        super(id);
+        this.email = email;
+        this.login = login;
+        this.name = name;
+        this.birthday = birthday;
+    }
 }
