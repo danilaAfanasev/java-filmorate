5b2fc5626e95c156c00633f2345a4c5257efbdbb
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-09-27 16:33:27 +0300

    Изменен доступ поля id на protected

--- src/main/java/ru/yandex/practicum/filmorate/model/Model.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Model.java
@@ -1,8 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public abstract class Model {
-    private int id;
+    protected int id;
+
+    public Model(int id) {
+        this.id = id;
+    }
 }
