38def52dae87c0836b77d83668368c08274956ec
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-09-27 16:33:59 +0300

    Добавлен конструктор с полем id из класса предка

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -8,15 +8,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @Builder(toBuilder = true)
 @AllArgsConstructor
+@NoArgsConstructor
 public class Film extends Model {
-
-    private int id;
 
     @NotBlank
     private String name;
@@ -30,4 +30,12 @@ public class Film extends Model {
 
     @PositiveOrZero
     private int duration;
+
+    public Film(int id, String name, String description, LocalDate releaseDate, int duration) {
+        super(id);
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+    }
 }
