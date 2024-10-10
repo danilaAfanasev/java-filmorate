35b8e9c944257cddcadff23df90c3c0f0fc3c020
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-10-10 17:29:26 +0300

    Обновлен модификатор доступа переменной UserService userService на защищенный

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController extends Controller<User> {
 
-    UserService userService;
+    protected UserService userService;
 
     @Autowired
     public UserController(UserService userService) {
