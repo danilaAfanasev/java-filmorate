249a5407a8bbdad890f8ad71556df5dadbb381fa
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-10-10 17:30:02 +0300

    Перемещен класс ErrorHandler в пакет exceptions

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ErrorHandler.java
@@ -1,13 +1,10 @@
-package ru.yandex.practicum.filmorate.controller;
+package ru.yandex.practicum.filmorate.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
-import ru.yandex.practicum.filmorate.exceptions.ValidationException;
-import ru.yandex.practicum.filmorate.model.ErrorResponse;
 
 @Slf4j
 @RestControllerAdvice
