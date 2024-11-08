a6d9540deeb9935c38aa55a043a6d7a1f3a5e699
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-11-08 12:44:22 +0300

    Обновлен интерфейс UserStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/UserStorage.java
@@ -1,6 +1,17 @@
-package ru.yandex.practicum.filmorate.storage;
+package ru.yandex.practicum.filmorate.storage.interfaces;
 
 import ru.yandex.practicum.filmorate.model.User;
+import java.util.Collection;
 
-public interface UserStorage extends Storage<User> {
+public interface UserStorage extends FriendsStorage {
+
+    Collection<User> getAll();
+
+    User create(User user);
+
+    User update(User user);
+
+    String delete(int id);
+
+    User getById(Integer id);
 }
