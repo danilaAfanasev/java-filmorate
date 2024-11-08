0eb994d3117fb135480488af36ce218638ab7bde
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-11-08 12:44:11 +0300

    Обновлен интерфейс FilmStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/FilmStorage.java
@@ -1,6 +1,21 @@
-package ru.yandex.practicum.filmorate.storage;
+package ru.yandex.practicum.filmorate.storage.interfaces;
 
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
+import java.util.Collection;
+import java.util.Set;
 
-public interface FilmStorage extends Storage<Film> {
+public interface FilmStorage extends LikesStorage {
+
+    Collection<Film> getAll();
+
+    Film create(Film film);
+
+    Film update(Film film);
+
+    String delete(int id);
+
+    Film getById(Integer id);
+
+    void addGenre(int filmId, Set<Genre> genres);
 }
