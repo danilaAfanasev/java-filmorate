8bd4f09c253e823c06817dc280a6f410bb1b8489
Danila Afanasyev <danilaafanasev146@gmail.com>
2024-11-08 12:47:59 +0300

    Обновлены тесты

--- src/test/java/ru/yandex/practicum/filmorate/model/FilmTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/model/FilmTest.java
@@ -2,17 +2,13 @@ package ru.yandex.practicum.filmorate.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-
 import java.time.LocalDate;
-import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -23,9 +19,14 @@ class FilmTest {
     private TestRestTemplate restTemplate;
 
     @Test
-    @DisplayName("Создать фильм с пустым названием")
+    @DisplayName("Создать фильм с пустым именем")
     void createFilmWithEmptyName() {
-        Film film = new Film(100, null, "Описание", LocalDate.now().minusYears(50), -100);
+        Film film = Film.builder()
+                .name(null)
+                .description("Интересный")
+                .releaseDate(LocalDate.now().minusYears(20))
+                .duration(100)
+                .build();
         ResponseEntity<Film> response = restTemplate.postForEntity("/films", film, Film.class);
 
         assertEquals("400 BAD_REQUEST", response.getStatusCode().toString());
@@ -34,8 +35,13 @@ class FilmTest {
     @Test
     @DisplayName("Создать фильм с длинным описанием")
     void createFilmWithTooLongDescription() {
-        String description = "Описание".repeat(200);
-        Film film = Film.builder().name("Фильм").description(description).releaseDate(LocalDate.now().minusYears(20)).duration(200).build();
+        String description = "аааааа".repeat(200);
+        Film film = Film.builder()
+                .name("Фильм")
+                .description(description)
+                .releaseDate(LocalDate.now().minusYears(20))
+                .duration(100)
+                .build();
         ResponseEntity<Film> response = restTemplate.postForEntity("/films", film, Film.class);
 
         assertEquals("400 BAD_REQUEST", response.getStatusCode().toString());
@@ -44,7 +50,12 @@ class FilmTest {
     @Test
     @DisplayName("Создать фильм с отрицательной длительностью")
     void createFilmWithMinusDuration() {
-        Film film = new Film(100, "Фильм", "Описание", LocalDate.now().minusYears(20), -100);
+        Film film = Film.builder()
+                .name("Фильм")
+                .description("Интересный")
+                .releaseDate(LocalDate.now().minusYears(20))
+                .duration(-100)
+                .build();
         ResponseEntity<Film> response = restTemplate.postForEntity("/films", film, Film.class);
 
         assertEquals("400 BAD_REQUEST", response.getStatusCode().toString());
@@ -53,26 +64,42 @@ class FilmTest {
     @Test
     @DisplayName("Обновить фильм с пустым названием")
     void updateFilmWithEmptyName() {
-        Film film = new Film(100, "Фильм", "Описание", LocalDate.now().minusYears(20), 100);
+        Film film = Film.builder()
+                .name("Фильм")
+                .description("Интересный")
+                .releaseDate(LocalDate.now().minusYears(20))
+                .duration(100)
+                .build();
         ResponseEntity<Film> response = restTemplate.postForEntity("/films", film, Film.class);
-        Film film2 = new Film(1, null, "Описание", LocalDate.now().minusYears(20), 100);
+        Film film2 = Film.builder()
+                .name(null)
+                .description("Интересный")
+                .releaseDate(LocalDate.now().minusYears(20))
+                .duration(100)
+                .build();
         HttpEntity<Film> entity = new HttpEntity<>(film2);
         ResponseEntity<Film> response2 = restTemplate.exchange("/films", HttpMethod.PUT, entity, Film.class);
 
         assertEquals("400 BAD_REQUEST", response2.getStatusCode().toString());
-
-        ResponseEntity<Collection<Film>> response3 = restTemplate.exchange("/films", HttpMethod.GET, null,
-                new ParameterizedTypeReference<>() {
-                });
     }
 
     @Test
     @DisplayName("Обновить фильм с длинным описанием")
     void updateFilmWithTooLongDescription() {
-        Film film = new Film(100, "Фильм", "Описание", LocalDate.now().minusYears(30), 100);
+        Film film = Film.builder()
+                .name("Фильм")
+                .description("Интересный")
+                .releaseDate(LocalDate.now().minusYears(20))
+                .duration(100)
+                .build();
         restTemplate.postForLocation("/films", film);
-        String description = "Описание".repeat(200);
-        Film film2 = Film.builder().name("Фильм").description(description).releaseDate(LocalDate.now().minusYears(20)).duration(100).build();
+        String description = "аааааа".repeat(200);
+        Film film2 = Film.builder()
+                .name("Фильм")
+                .description(description)
+                .releaseDate(LocalDate.now().minusYears(20))
+                .duration(100)
+                .build();
         HttpEntity<Film> entity = new HttpEntity<>(film2);
         ResponseEntity<Film> response2 = restTemplate.exchange("/films", HttpMethod.PUT, entity, Film.class);
 
@@ -82,9 +109,19 @@ class FilmTest {
     @Test
     @DisplayName("Обновить фильм с отрицательной длительностью")
     void updateFilmWithMinusDuration() {
-        Film film = new Film(100, "Фильм", "Описание", LocalDate.now().minusYears(20), 100);
+        Film film = Film.builder()
+                .name("Фильм")
+                .description("Интересный")
+                .releaseDate(LocalDate.now().minusYears(20))
+                .duration(100)
+                .build();
         ResponseEntity<Film> response = restTemplate.postForEntity("/films", film, Film.class);
-        Film film2 = new Film(1, "Фильм", "Описание", LocalDate.now().minusYears(20), -100);
+        Film film2 = Film.builder()
+                .name("Фильм")
+                .description("Интересный")
+                .releaseDate(LocalDate.now().minusYears(20))
+                .duration(-100)
+                .build();
         HttpEntity<Film> entity = new HttpEntity<>(film2);
         ResponseEntity<Film> response2 = restTemplate.exchange("/films", HttpMethod.PUT, entity, Film.class);
 
